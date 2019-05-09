2e49f4ec0db1f211dffb1caa122ac237e057f3fb
Leopiron <sourovsarkar84@gmail.com>
2019-05-09 22:52:36 +0530

    Command line added

---
+++ Learning/Command_line.java
@@ -0,0 +1,17 @@
+
+/**
+ * Command line argument.
+ *
+ * @author (Leoperon)
+ * @version (09.05.2019)
+ */
+public class Command_line
+{
+    public static void main(String[] args)
+    {
+        for(String t: args)
+        {
+           System.out.println(t);
+        }
+    }
+}
