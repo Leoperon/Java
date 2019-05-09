2e8393ed2649d1baf934cbfe00210f37c9330cbd
Leopiron <sourovsarkar84@gmail.com>
2019-05-09 22:56:26 +0530

    Print integer added

---
+++ Learning/Print_Integers.java
@@ -0,0 +1,18 @@
+
+/**
+ *  Printing Integers from 1-10 using for loop.
+ *
+ * @author (Leoperon)
+ * @version (09.05.2019)
+ */
+public class Print_Integers
+{
+    public static void main(String[] arguments)
+    {
+        int c;//Declaring a variables
+        for(c=1;c<=10;c++)
+        {
+            System.out.println(c);
+        }
+    }
+}
