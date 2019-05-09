05df2f2dfabcf13e43ad9c78b1a5baa6cd434b1e
Leopiron <sourovsarkar84@gmail.com>
2019-05-09 22:49:54 +0530

    Display added

---
+++ Learning/Display.java
@@ -0,0 +1,14 @@
+
+/**
+ * Display here.
+ *
+ * @author (Leoperon)
+ * @version (09.05.2019)
+ */
+public class Display
+{
+    public static void main(String[] arguments)
+    {
+        System.out.println("Let's do something using java technology.");
+    }
+}
