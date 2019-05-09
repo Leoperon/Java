24a010689ad5bb6b927e33eddabf043a8f4f87e2
Leopiron <sourovsarkar84@gmail.com>
2019-05-09 22:54:23 +0530

    If else added

---
+++ Learning/If_else.java
@@ -0,0 +1,21 @@
+
+/**
+ *  If else control instructions.
+ *
+ * @author (Leoperon)
+ * @version (09.05.2019)
+ */
+public class If_else
+{
+    public static void main(String[] args) 
+    {
+        boolean learning =true;
+        if(learning){
+            System.out.println("Java Programmer");
+        }
+        else
+        {
+           System.out.println("What are you doing here?"); 
+        }
+    }
+}
