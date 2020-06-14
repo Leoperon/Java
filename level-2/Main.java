ddb9ec096e94ca8b687ac3f9c15a546f987ffc1e
Leopiron <sourovsarkar84@gmail.com>
2020-06-14 23:27:44 +0530

    File Added

---
+++ level-2/Main.java
@@ -0,0 +1,25 @@
+class Main {
+  public static void main(String[] args) {
+    // Assign a list of numbers into the numbers variable
+    
+    int[] numbers={1, 4, 6, 9, 13, 16};
+    int oddSum = 0;
+    int evenSum = 0;
+    for(int number:numbers)
+    {
+      if(number%2==0)
+      {
+        evenSum +=number;
+      }
+      else
+      {
+        oddSum +=number;
+      }
+    }
+    // Calculate the sum of odd numbers and the sum of even numbers
+    
+
+    System.out.println("The sum of odd numbers: " + oddSum);
+    System.out.println("The sum of even numbers: " + evenSum);
+  }
+}
